e25caf82b69c8415ba739c24dd1812b7327193f3
Oscar Araque <oscar.aiborra@gmail.com>
2016-04-15 13:37:27 +0200

    sentiment140 complete

---
+++ sentiment140/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	INPUT="$(pwd)/../data/mydata"
+else
+	INPUT=$1
+fi
+
+if [ -z "$2" ]
+then
+	OUTPUT="$(pwd)/../data/sentiment140/myout.txt"
+else
+	OUTPUT=$2
+fi
+
+echo "Input from $INPUT"
+echo "Output to $OUTPUT"
+echo ""
+
+
+docker run --rm \
+	-v $INPUT:/input.txt \
+	-v $OUTPUT:/output.txt \
+	sentiment140:latest
