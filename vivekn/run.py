9669fe172bbc3b1abd9fbdab4e4bb28b17741e22
Oscar Araque <oscar.aiborra@gmail.com>
2016-04-21 19:57:05 +0200

    vivekn output adapted to general format

--- vivekn/run.py
+++ vivekn/run.py
@@ -3,6 +3,14 @@ import requests
 import csv
 import sys
 
+def polarity(s):
+    if s == 'Positive':
+        return 1
+    elif s == 'Negative':
+        return -1
+    elif s == 'Neutral':
+        return 0
+
 with open(sys.argv[2], 'w') as fout:
     writer = csv.writer(fout, delimiter='\t')
     with open(sys.argv[1], 'r') as fin:
@@ -11,4 +19,5 @@ with open(sys.argv[2], 'w') as fout:
                                 data=json.dumps(sentences))
         for i,result in enumerate(response.json()):
             string = sentences[i].strip()
-            writer.writerow([result['result'], string])
+            prediction = polarity(result['result'])
+            writer.writerow([i, prediction])
